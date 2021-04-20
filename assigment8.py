531b762880df42aecb68855ccd9a608893e8dd42
enesgunduz-zz <gunduzzenes7@gmail.com>
2021-04-20 22:53:23 +0300

    emre ile denedik

---
+++ assigment8.py
@@ -0,0 +1,9 @@
+name = input("Please enter your username: ")
+name1= "Joseph"
+password= "W@12"
+
+if name==name1:
+    print(f"Hello, {name1}! The password is {password}:")
+
+else:
+    print(f"Hello, {name}! See you later.")
