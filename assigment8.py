08e9ca1d5e73a7286d090d51f0c54c66aedb237d
enesgunduz-zz <38518155+enesgunduz-zz@users.noreply.github.com>
2021-04-20 23:49:45 +0300

    Delete assigment8.py

--- assigment8.py
+++
@@ -1,9 +0,0 @@
-name = input("Please enter your username: ")
-name1= "Joseph"
-password= "W@12"
-
-if name==name1:
-    print(f"Hello, {name1}! The password is {password}:")
-
-else:
-    print(f"Hello, {name}! See you later.")
